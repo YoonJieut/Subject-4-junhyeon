6fee0e259c8578d5fcf2e9e0819f2bebc59dbaa5
YoonJunHyeon <qmdldos@gmail.com>
2023-10-13 16:26:42 +0900

    writefile 모듈 설명 추가, 각 매개변수와 함수의 리턴값 정리, 테스트 전 저장

--- modules/writefile.js
+++ modules/writefile.js
@@ -4,10 +4,13 @@ const pureslide = require('./pureslide');
 const styleTag = require('./styleTag');
 const elementScript = require('./elementScript');
 
+// styleTag : 3개 매개변수, 각각 슬라이드의 배경색, 문자열입력
+// Html 템플릿 : 3개의 매개변수,
+// 1. styleTag함수, 2. elmentScript문자열 3.pureslide 문자열
 
 
 const fileName = ["index.html", "sub.html"];
-fs.writeFile(`index.html`, HTMLTemplate(styleTag, elementScript ,pureslide), (err) => {
+fs.writeFile(`index.html`, HTMLTemplate(styleTag("red","blue","yellow"), elementScript ,pureslide), (err) => {
   if(err) {
     console.error("에러발생");
   } else {
