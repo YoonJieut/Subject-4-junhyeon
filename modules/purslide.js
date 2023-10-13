a677da809122da0ea31409bf2d1d3fda21f347cc
YoonJunHyeon <qmdldos@gmail.com>
2023-10-13 11:38:38 +0900

    로직 텍스트 수정

--- modules/purslide.js
+++ modules/purslide.js
@@ -2,30 +2,34 @@
 // ! 첫 요소 삭제 및 뒤로 이동
 // ! 단 slideUl을 선언
 
-let appnedSomething = ()=>{
-  let remove1st = slideUl.removeChild(slideUl.children[0]);
-  slideUl.appendChild(remove1st);
-  slideUl.style.left = 0; // 초기화 작업
-}
-
-// * 스르륵 height 만큼, 위로 이동하는 것
 let topMove= (height) => {
   let temp = 0;
   let topMoving = setInterval(()=>{
     if(temp > -height) {
-      temp = temp -7;
+      temp = temp -5;
       slideUl.style.top = temp+"px";
     } else {
       clearInterval(topMoving)
     }
   }, 0.5)
 }
-// 최종 조립 
-  setInterval(()=>{
-    topMove(700);
-    setTimeout(()=>{
-      appnedSomething();
-    },1000);
-  }, 5000)
+
+// ! 첫 요소 삭제 및 뒤로 이동
+let appnedSomething = ()=>{
+  let remove1st = slideUl.removeChild(slideUl.children[0]);
+  slideUl.appendChild(remove1st);
+  slideUl.style.top = 0;
+}
+
+//최종 조립
+setTimeout(()=>{
+  // 시작 전 타임아웃
+    setInterval(()=>{
+      topMove(400);
+      setTimeout(()=>{
+        appnedSomething();
+      },500);
+    }, 5000)
+  },5000)
 
 module.exports = slideLogic;
