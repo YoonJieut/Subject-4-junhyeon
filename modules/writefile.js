d58bf3d43e2d5693925ad48d3ac0503bbe946c3f
YoonJunHyeon <qmdldos@gmail.com>
2023-10-13 11:52:34 +0900

    html템플 분리 및 exports 및 require 테스트

--- modules/writefile.js
+++ modules/writefile.js
@@ -1,22 +1,8 @@
 const fs = require('fs');
+const HTMLTemplate = require('./HTMLTemplate');
 
-  let template = `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test</title>
-  </head>
-  <body>
-    <h1>나는 슬라이드</h1>
-  </body>
-  </html>
-  `;
 
-// console.log(fileList);
-// console.log(fileList.length);
-
-fs.writeFile(`index.html`, template, (err) => {
+fs.writeFile(`index.html`, HTMLTemplate, (err) => {
   if(err) {
     console.error("에러발생");
   } else {
